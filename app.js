d2d5c1d290596022321e7a96dd1fa23025931f8f
deepatharun-s <deepatharun.s@mitrahsoft.in>
2026-03-26 12:30:55 +0530

    stash commit

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 console.log("Welcome to MitrahSoft im from another branch im deepatharun and im kiran kumar from cbe we are TL of the CBE branch");
 
 console.log("Today we learnt about git Merge");
+
+
+"Today learnt about git stash"
